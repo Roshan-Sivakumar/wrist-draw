b48f9b82ebfce7bc23d5d7c4f0e6b542883cfe45
Roshan <65855350+Roshan-Sivakumar@users.noreply.github.com>
2022-04-12 12:23:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
-function setup(){
+leftWrist_x = 0;
+rightWrist_x = 0;
+difference = 0;
+
+function setup() {
     video = createCapture(VIDEO);
-    video.size(400,400);
-    video.position(10,50);
+    video.size(400, 400);
+    video.position(10, 50);
 
-    canvas = createCanvas(800,400);
-    canvas.position(430,100);
+    canvas = createCanvas(800, 400);
+    canvas.position(430, 130);
 
-    poseNet = ml5.poseNet(video,modelDone);
-    poseNet.on('pose',gotposes);
+    poseNet = ml5.poseNet(video, modelDone);
+    poseNet.on('pose', gotpose);
 }
 
-function draw(){
-    background("#5196e3");
+function draw() {
+    background("grey");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = " + difference + "px";
+    textSize(difference);
+    fill("red");
+    text('Roshan', 100, 300);
 }
 
-function modelDone(){
-    console.log("PoseNet Is Initialized And Loaded");
+function modelDone() {
+    console.log("model loaded");
 }
 
-function gotposes(results,error){
-    if(error){
-        console.error(error);
-    }
-    if(results.length > 0){
+function gotpose(results) {
+    if (results.length > 0) {
         console.log(results);
 
-        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
-        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist_x - rightWrist_x);
+
+        console.log("rightWrist_x = " + results[0].pose.rightWrist.x + " rightWrist_y = " + results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = " + results[0].pose.leftWrist.x + " leftWrist_y = " + results[0].pose.leftWrist.y);
     }
 }
